28d0fb40d8e40a445c142885e8c963c00064bb6e
Jongho Jeon <hojong.jeon@gmail.com>
2020-03-21 22:42:17 +0900

    Renamed Spring boot app

--- src/main/kotlin/com/hojong/Main.kt
+++ src/main/kotlin/com/hojong/Main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootR2dbcExampleApplication
+class App
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootR2dbcExampleApplication>(*args)
+	runApplication<App>(*args)
 }
