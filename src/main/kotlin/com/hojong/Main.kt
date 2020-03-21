7891f5ace6271d814d41b8377c4bdaf61926ae57
Jongho Jeon <hojong.jeon@gmail.com>
2020-03-21 21:47:51 +0900

    Initial commit

---
+++ src/main/kotlin/com/hojong/Main.kt
@@ -0,0 +1,11 @@
+package com.hojong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootR2dbcExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootR2dbcExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/hojong/MainTest.kt
@@ -0,0 +1,13 @@
+package com.hojong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
